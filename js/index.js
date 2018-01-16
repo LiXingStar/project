74eabb74fc67b7f943d47d2afae8a474e2286d18
lixing <1459294281@qq.com>
2018-01-17 00:02:03 +0800

    'adddd'

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 20,
+        scrollbar: '.swiper-scrollbar',
+        scrollbarHide: true,
+    });
+});
